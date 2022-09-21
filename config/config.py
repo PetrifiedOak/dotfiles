2585ed1ea9bf8b029e8276483864de5de99f1488
oak <oak@alpine.home>
2022-09-21 22:15:44 +0200

    added my qutebrowser config.py

---
+++ config/config.py
@@ -0,0 +1,4 @@
+c.url.start_pages = ['https://petrifiedoak.neocities.org/startpage.html']
+config.load_autoconfig(False)
+c.url.searchengines = {"DEFAULT": "https://librex.extravi.dev/search.php?q={}"}
+config.source('gruvbox.py')
