31b0f51d3159ddd0499e361e1b86c0a2140546b1
oak <oak@alpine.home>
2022-09-30 15:40:13 +0200

    Added better fg color

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -10,8 +10,8 @@ static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray3[]       = "#ebdbb2";
+static const char col_gray4[]       = "#ebdbb2";
 static const char col_cyan[]        = "#32302f";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
